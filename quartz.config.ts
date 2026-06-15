27207824eeffb27fd35b082277a8a1fce6ea48a5
QinAnze <qaz15205287785@outlook.com>
2026-06-15 23:53:10 +0800

    init quartz site

---
+++ quartz.config.ts
@@ -0,0 +1,75 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "🪴 Quartz 4.0",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    baseUrl: "quartz.jzhao.xyz",
+    ignorePatterns: ["private", "templates"],
+    theme: {
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#f8fbff",
+          lightgray: "#e2e8f0",
+          gray: "#94a3b8",
+          darkgray: "#334155",
+          dark: "#0f172a",
+          secondary: "#2563eb",
+          tertiary: "#3b82f6",
+          highlight: "rgba(59, 130, 246, 0.08)",
+        },
+        darkMode: {
+          light: "#0f172a",
+          lightgray: "#1e293b",
+          gray: "#64748b",
+          darkgray: "#cbd5e1",
+          dark: "#f8fafc",
+          secondary: "#60a5fa",
+          tertiary: "#93c5fd",
+          highlight: "rgba(96, 165, 250, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.TableOfContents(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
+      }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+    ],
+  },
+}
+
+export default config

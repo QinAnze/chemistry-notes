8428904b8bfc3daba425fe8c1069a2ef0a04a0f4
QinAnze <qaz15205287785@outlook.com>
2026-06-27 23:05:10 +0800

    Fix title link with data-router-ignore (full page nav), PDF links with download
    - PageTitle: add data-router-ignore so SPA doesn't intercept, browser
      does a full page load to the homepage
    - PDF index: add download attribute to force file download instead of
      SPA interception

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -4,7 +4,7 @@ function PageTitle({ cfg }: QuartzComponentProps) {
   const title = cfg?.pageTitle ?? "Untitled Quartz"
   return (
     <h1 class="page-title">
-      <a href="https://qinanze.github.io/chemistry-notes/">{title}</a>
+      <a href="https://qinanze.github.io/chemistry-notes/" data-router-ignore>{title}</a>
     </h1>
   )
 }
